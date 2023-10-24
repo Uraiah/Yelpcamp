7c4517b13668dabc4c35e045e9e724ef2b29b4eb
gregoryantoine90@gmail.com <gregoryantoine90@gmail.com>
2023-10-24 16:48:30 -0400

    reseeded database Tuesday October 24th, 2023 4:47pm

--- seeds/index.js
+++ seeds/index.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose'); //Tuesday April 20th 2022 4:44pm
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');//I forgot to add this one. 4/27/2022
 const Campground = require('../models/campground');
+require("dotenv").config(); //Added Monday October 23rd, 2023 4:40pm
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+/*mongoose.connect('mongodb://localhost:27017/yelp-camp', { //Removed: Thursday October 18th, 2023 4:32pm
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-});
+});*/
+
+//Copied and Added Thursday October 19th, 2023 4:32pm
+    // In the seeds/index.js file, modify it so it's set up like this: 
+     
+    require('dotenv').config();
+     
+    console.log(process.env.DB_URL); // make sure that process.env.DB_URL is set correctly in the seeds/index.js file as well
+     
+    const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+     
+    mongoose.connect(dbUrl); // do not pass any additional options in the mongoose.connect call 
+    //Ended here Added Thursday October 19th, 2023 4:41pm
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -22,7 +35,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '63b4a1d89cfa4d4ca85f5627' ,//, '5f5c330c2cd79d538f2c66d9' 63c7189352fe956154d925a2 '63d59b91a5143242b0e84c46'
+            author: '65270a71eba835fb3dcb77f8',//Added Monday October 23rd, 2023 5:09pm //'63b4a1d89cfa4d4ca85f5627' Removed and Added Monday October 23rd, 2023 5:09pm,//, '5f5c330c2cd79d538f2c66d9' 63c7189352fe956154d925a2 '63d59b91a5143242b0e84c46'
             location: `${cities[random1000].city},'63cf0586526b114fe8dc0627' ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251', //Tuesday May 31st 2022 4:39pm
@@ -83,3 +96,44 @@ const seedDB = async () => {
 seedDB().then(() => { //Monday May 2nd 2022 4:52pm?
     mongoose.connection.close();
 });
+/*
+  title: 'Test',
+  geometry: {
+    type: 'Point',
+    coordinates: [ -100.459284161645, 47.4443684021068 ]
+  },
+  price: 100,
+  description: 'Places',
+  location: 'North Dakoda',
+  reviews: [],
+  _id: new ObjectId("653199322c865a44fdb576ae"),
+  images: [
+    {
+      url: 'https://res.cloudinary.com/dm4lfb0xf/image/upload/v1697749292/YelpCamp/qwsnwzkzuiowzkwh0a5g.jpg',
+      filename: 'YelpCamp/qwsnwzkzuiowzkwh0a5g',
+      _id: new ObjectId("653199322c865a44fdb576af")
+    },
+    {
+      url: 'https://res.cloudinary.com/dm4lfb0xf/image/upload/v1697749293/YelpCamp/csvq1wm1suw1ylzkfvds.jpg',
+      filename: 'YelpCamp/csvq1wm1suw1ylzkfvds',
+      _id: new ObjectId("653199322c865a44fdb576b0")
+    },
+    {
+      url: 'https://res.cloudinary.com/dm4lfb0xf/image/upload/v1697749294/YelpCamp/kypxfikcx8ryomlkjajr.jpg',
+      filename: 'YelpCamp/kypxfikcx8ryomlkjajr',
+      _id: new ObjectId("653199322c865a44fdb576b1")
+    },
+    {
+      url: 'https://res.cloudinary.com/dm4lfb0xf/image/upload/v1697749295/YelpCamp/qgev9nsykqjxpid8hpu7.jpg',
+      filename: 'YelpCamp/qgev9nsykqjxpid8hpu7',
+      _id: new ObjectId("653199322c865a44fdb576b2")
+    },
+    {
+      url: 'https://res.cloudinary.com/dm4lfb0xf/image/upload/v1697749297/YelpCamp/bprowwy5sutgjlnnvbcc.jpg',
+      filename: 'YelpCamp/bprowwy5sutgjlnnvbcc',
+      _id: new ObjectId("653199322c865a44fdb576b3")
+    }
+  ],
+  author: new ObjectId("65270a71eba835fb3dcb77f8"),
+  __v: 0
+ */
